ff9b0589d1bf6e9885cf4199e55ba09a8eda8f98
Fetra01 <fetra.andri@outlook.fr>
2020-10-28 12:02:49 +0100

    Création du page Home et Not Found

---
+++ src/component/Navigation.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Navigation = () => {
+    return (
+        <div>
+            <div className="sidBar">
+                <div className="id">
+                    <div className="idContent">
+                        <h3>Fetra ANDRIAMAHALY</h3>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Navigation;
---
+++ src/pages/Home.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Navigation from '../component/Navigation';
+
+const Home = () => {
+    return (
+        <div className="home">
+            <Navigation />
+            <div className="homeContent">
+                <div className="content">
+                    <h1>Fetra ANDRIAMAHALY</h1>
+                    <h2>Développeur web et application</h2>
+                    <div className="pdf">
+                        <a href=".media/CV.pdf" target="_blank">Télécharger</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Home;
