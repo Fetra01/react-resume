1ec5f5c6cec98aa83c7af61c98a296785cb957e2
Fetra01 <fetra.andria@outlook.fr>
2020-11-01 17:25:07 +0100

    Pages contact et styles/contact finis

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,11 +1,55 @@
 import React from 'react';
 import Navigation from '../component/Navigation';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 
 const Contact = () => {
     return (
-        <div>
+        <div className="contact">
             <Navigation />
-            Page Contact !
+            <div className="contactContent">
+                <div className="header"></div>
+                <div className="contactBox">
+                    <h1>Contactez moi</h1>
+                    <ul>
+                        <li>
+                            <i className="fas fa-map-marker-alt"></i>
+                            <span>Montpellier</span>
+                        </li>
+                        <li>
+                            <i className="fas fa-mobile-alt"></i>
+                            <CopyToClipboard text="0664267886">
+                                <span className="clickInput" onClick={() => {alert('Téléphone copié ! ');}}> 06 64 26 78 86 </span>
+                            </CopyToClipboard>
+                        </li>
+                        <li>
+                            <i className="fas fa-envelope"></i>
+                            <CopyToClipboard text="f.andriamahaly@gmail.com">
+                                <span className="clickInput" onClick={ () => {alert('E-mail copié !');}}>f.andriamahaly@gmail.com</span>
+                            </CopyToClipboard>
+                        </li>
+                    </ul>
+                </div>
+                <div className="socialNetwork">
+                    <ul>
+                        <a href="http://www.google.com" target="_blank" rel="noopener noreferrer">
+                            <h4>LinkedIn</h4>
+                            <i className="fab fa-linkedin"></i>
+                        </a>
+                        <a href="http://www.google.com" target="_blank" rel="noopener noreferrer">
+                            <h4>Github</h4>
+                            <i className="fab fa-github"></i>
+                        </a>
+                        <a href="http://www.google.com" target="_blank" rel="noopener noreferrer">
+                            <h4>Twitter</h4>
+                            <i className="fab fa-twitter"></i>
+                        </a>
+                        <a href="http://www.google.com" target="_blank" rel="noopener noreferrer">
+                            <h4>CodePen</h4>
+                            <i className="fab fa-codepen"></i>
+                        </a>
+                    </ul>
+                </div>
+            </div>
         </div>
     );
 };
