b4bcebb2429e3276ddd9346546ba6879d8d53cf3
Fetra01 <fetra.andria@outlook.fr>
2020-11-01 16:19:47 +0100

    Pages portfolio finis

--- src/component/portfolio/ProjectList.js
+++ src/component/portfolio/ProjectList.js
@@ -4,21 +4,51 @@ import Project from './Project';
 
 class ProjectList extends Component {
     state = {
-        projects: portfolioData
+        radios: [
+            {id: 1, value: "javascript"},
+            {id: 2, value: "css"},
+            {id: 3, value: "react"},
+            {id: 4, value: "php"}
+        ],
+        projects: portfolioData,
+        selectRadio: "javascript"
+    }
+
+    handleRadio = (event) => {
+        let radio = event.target.value
+        this.setState({selectRadio: radio})
     }
 
     render() {
-        const {projects} = this.state;
+        let {projects, radios, selectRadio} = this.state;
 
         return (
             <div className="portfolioContent">
                 <ul className="radioDisplay">
-
+                    {
+                        radios.map((radio) => {
+                            return (
+                                <li key={radio.id}>
+                                   <input 
+                                        type="radio"
+                                        name="radio"
+                                        checked={radio.value === selectRadio}
+                                        value={radio.value}
+                                        id={radio.value}
+                                        onChange={this.handleRadio}
+                                    /> 
+                                    <label htmlFor={radio.value}>{radio.value}</label>
+                                </li>
+                            )
+                        })
+                    }
                 </ul>
 
                 <div className="projects">
                     {
-                        projects.map(item => {
+                        projects
+                        .filter(item => item.languages.includes(selectRadio))
+                        .map(item => {
                             return (
                                 <Project
                                     key={item.id}
