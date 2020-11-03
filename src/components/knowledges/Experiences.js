da37ff971a82cbf25b647ac753b0dd38ead51a5c
Fetra01 <fetra.andria@outlook.fr>
2020-11-03 13:34:58 +0100

    Mise en page des expériences

--- src/components/knowledges/Experiences.js
+++ src/components/knowledges/Experiences.js
@@ -5,19 +5,21 @@ const Experiences = () => {
         <div className="experiences">
             <h3>Expérience</h3>
             <div className="exp1">
-                <h4>Titre poste</h4>
-                <h5>Date du poste</h5>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum odio
-                optio a hic quaerat. Ea obcaecati aut voluptate odio alias nostrum facilis,
-                ducimus, repellat pariatur adipisci fugiat sequi exercitationem voluptatum.</p>
+                <h4>Développeur web | <a href="http://www.cigaloisirs.fr" target="_blank" rel="noopener noreferrer">Cigaloisirs</a></h4>
+                <h5>Juin-Août 2020</h5>
+                <p>Refonte du site web de la société Cigaloisir: 
+                    <br/>- Création de logo
+                    <br/>- Création du contenu</p>
             </div>  
 
             <div className="exp2">
-                <h4>Titre poste</h4>
-                <h5>Date du poste</h5>
-                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad facere
-                cumque ex veniam fuga asperiores consequuntur, necessitatibus perferendis
-                est aliquam ducimus autem id nemo officia praesentium, ipsam iste dolorum eveniet.</p>
+                <h4>Stagiaire Développeur web | <a href="http://www.emity.io" target="_blank" rel="noopener noreferrer">Emity</a></h4>
+                <h5>Septembre-Décembre 2019</h5>
+                <p>Création d'une application web:
+                    <br/>- Développer la partie Back-end 
+                    <br/>- Développer la partie Front-end 
+                    <br/>- Utillisation de l' API SNCF 
+                </p>
             </div>          
         </div>
     );
