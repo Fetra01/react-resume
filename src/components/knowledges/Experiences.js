7e2ac92c4f5822ef27256d9a134ca8ecd9ef35fa
Fetra01 <fetra.andria@outlook.fr>
2020-11-04 10:26:42 +0100

    Instalation de typewrite, changement du

--- src/components/knowledges/Experiences.js
+++ src/components/knowledges/Experiences.js
@@ -9,10 +9,18 @@ const Experiences = () => {
                 <h5>Juin-Août 2020</h5>
                 <p>Refonte du site web de la société Cigaloisir: 
                     <br/>- Création de logo
-                    <br/>- Création du contenu</p>
+                    <br/>- Création des contenu</p>
             </div>  
 
             <div className="exp2">
+                <h4>Développeur web | <a href="http://www.cigaloisirs.fr" target="_blank" rel="noopener noreferrer">Cigaloisirs</a></h4>
+                <h5>Juin-Août 2020</h5>
+                <p>Refonte du site web de la société Cigaloisir: 
+                    <br/>- Création de logo
+                    <br/>- Création du contenu</p>
+            </div>  
+
+            <div className="exp3">
                 <h4>Stagiaire Développeur web | <a href="http://www.emity.io" target="_blank" rel="noopener noreferrer">Emity</a></h4>
                 <h5>Septembre-Décembre 2019</h5>
                 <p>Création d'une application web:
@@ -20,7 +28,7 @@ const Experiences = () => {
                     <br/>- Développer la partie Front-end 
                     <br/>- Utillisation de l' API SNCF 
                 </p>
-            </div>          
+            </div>         
         </div>
     );
 };
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import Navigation from '../components/Navigation';
+import Navigation from '../components/Navigation'; 
+import Typewriter from 'typewriter-effect';
 
 const Home = () => {
     return (
@@ -7,8 +8,46 @@ const Home = () => {
             <Navigation />
             <div className="homeContent">
                 <div className="content">
-                    <h1>Fetra ANDRIAMAHALY</h1>
-                    <h2>Développeur web et application</h2>
+                    <h1>
+                      Bienvenue
+                    </h1>
+                    <h2> 
+                        <Typewriter 
+                            onInit={(typewriter) => {
+                                typewriter
+                                    .typeString("Moi c'est Fetra,")
+                                    .pauseFor(2500)
+                                    .typeString(" Développeur Web et application")
+                                    .pauseFor(1500)
+                                    .deleteChars(18)
+                                    .typeString("React")
+                                    .start();
+                            }}
+                           
+                        />
+                        {/*<Typewriter
+                            onInit={(typewriter) => {
+                                typewriter.typeString('Hello World!')
+                                .callFunction(() => {
+                                    console.log('String typed out!');
+                                })
+                                .pauseFor(2500)
+                                .deleteAll()
+                                .callFunction(() => {
+                                    console.log('All strings were deleted');
+                                })
+                                .start();
+                             }}
+                        />
+                        <Typewriter
+                            options={{
+                                strings: ['Hello', 'World'],
+                                autoStart: true,
+                                loop: true,
+                            }}
+                        />*/}
+                    </h2>
+
                     <div className="pdf">
                         <a href="./media/CV.pdf" target="_blank" >Télécharger CV</a>
                     </div>
