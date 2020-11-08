72954d44cd719f7dc3c24d82e4880e2ea5955b7e
Fetra01 <fetra.andria@outlook.fr>
2020-11-08 20:30:41 +0100

    Création du components info(à-propos) et mise en form du css

---
+++ src/components/Info.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+
+class Info extends Component {
+    state = {
+        show: false
+    };
+
+    handleShow = () => {
+        this.setState({
+            show:!this.state.show
+        })
+    };
+    render() {
+      return (
+            <div className="info">
+                <div className="button" onClick={this.handleShow}>A propos</div>
+            {
+                this.state.show && (
+                    <div className="show">
+                        <div className="showContent">
+                            <h2>A propos de moi</h2>
+                            <p className="text">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Placeat similique id dolore ullam fugit quaerat dignissimos ducimus est voluptate libero itaque natus nulla mollitia, cum a. Esse odit eaque delectus?Commodi maiores laboriosam nostrum repellendus unde architecto iste fugiat nihil voluptatum ex? Vitae rem quo repellendus ipsa consequatur alias natus a autem aperiam, ex suscipit maxime dicta eos facere ea?</p>
+                            <div className="button return" onClick={this.handleShow}>Retour</div>
+                        </div>
+                    </div>
+                )
+            }
+            
+            </div>            
+        );
+    }
+}
+
+export default Info;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Navigation from '../components/Navigation'; 
-import Typewriter from 'typewriter-effect';
+//import Typewriter from 'typewriter-effect';
+import Info from '../components/Info'
 
 const Home = () => {
     return (
@@ -8,31 +9,36 @@ const Home = () => {
             <Navigation />
             <div className="homeContent">
                 <div className="content">
-                    <h1>
-                      Bienvenue
-                    </h1>
-                    <h2> 
-                        <Typewriter 
-                            onInit={(typewriter) => {
-                                typewriter
-                                    .typeString("Moi c'est Fetra,")
-                                    .pauseFor(2500)
-                                    .typeString(" Développeur Web et application")
-                                    .pauseFor(1500)
-                                    .deleteAll()
-                                    .typeString(' Je Développes en Php/Symfony')
-                                    .pauseFor(1500)
-                                    .deleteChars(11)
-                                    .typeString(' Js/React')
-                                    .start()
-                                    .deleteAll()
-                            }}
-                        />
-                    </h2>
-
-                    <div className="pdf">
-                        <a href="./media/CV.pdf" target="_blank" >Télécharger CV</a>
+                    <div className="title">                   
+                        <h1>
+                        Bienvenue
+                        </h1>
+                        <h2> 
+                            {/*<Typewriter 
+                                onInit={(typewriter) => {
+                                    typewriter
+                                        .typeString("Moi c'est Fetra,")
+                                        .pauseFor(3000)
+                                        .typeString(" Développeur Web et application")
+                                        .pauseFor(1500)
+                                        .deleteAll()
+                                        .typeString(' Je Développes en Php/Symfony')
+                                        .pauseFor(1500)
+                                        .deleteChars(11)
+                                        .typeString(' Js/React')
+                                        .start()
+                                        .deleteAll()
+                                }}
+                            />*/}Hello
+                        </h2>
                     </div>
+                    <div className="contentInfo">
+                        <Info />
+                        <div className="pdf">
+                            <a href="./media/CV.pdf" target="_blank" >Télécharger CV</a>
+                        </div>
+                    </div>
+                    
                 </div>
             </div>
         </div>
